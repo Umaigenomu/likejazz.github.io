4c1b478eaf1f47b7c4917f9ed5d6c4d73b991668
Sang-Kil Park <kaon.park@daumkakao.com>
2017-03-21 17:18:58 +0900

    Publishing

--- publish.sh
+++ publish.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+cp /Users/kaonpark/workspace/github.com/likejazz/likejazz.github.io.wiki/* public/
+rm -rf public/_Footer.md
+rm -rf public/_Sidebar.md
 git add . && git commit -m "Publishing" && git push origin master
