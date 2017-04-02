2f7a6d6fe5364bd77bff997d38a11f918865f103
Sang-Kil Park <kaon.park@daumkakao.com>
2017-04-02 11:04:06 +0900

    Publishing

--- publish.sh
+++ publish.sh
@@ -6,12 +6,6 @@ GITHUB_PATH=/Users/kaonpark/workspace/github.com/likejazz
 cd ${GITHUB_PATH}/likejazz.github.io.wiki
 git commit -a -m "Publishing" && git push origin master
 
-# Copy wiki docs to main repository and remove unnecessary files.
-rm -rf ${GITHUB_PATH}/likejazz.github.io/public/*.md
-cp * ${GITHUB_PATH}/likejazz.github.io/public/
-cd ${GITHUB_PATH}/likejazz.github.io/public
-rm -rf _Sidebar.md _Footer.md
-
 # Github.io publishing
-cd ..
+cd ${GITHUB_PATH}/likejazz.github.io
 git add . && git commit -m "Publishing" && git push origin master
