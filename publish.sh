f8305bcf9b19c4b8fb763d3db1bbc26aac79fcf9
Sang-Kil Park <kaon.park@daumkakao.com>
2017-04-02 11:06:07 +0900

    Publishing

--- publish.sh
+++ publish.sh
@@ -3,9 +3,15 @@
 GITHUB_PATH=/Users/kaonpark/workspace/github.com/likejazz
 
 # Wiki publishing
+echo "-------------------------"
+echo "Wiki Publishing"
+echo "-------------------------"
 cd ${GITHUB_PATH}/likejazz.github.io.wiki
 git commit -a -m "Publishing" && git push origin master
 
 # Github.io publishing
+echo "-------------------------"
+echo "Github.io Publishing"
+echo "-------------------------"
 cd ${GITHUB_PATH}/likejazz.github.io
 git add . && git commit -m "Publishing" && git push origin master
