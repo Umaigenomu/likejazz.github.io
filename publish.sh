dfbc9db7e7b935a6da0ac060c669446cba4eae46
Sang-Kil Park <kaon.park@daumkakao.com>
2017-04-09 17:23:49 +0900

    Publishing

--- publish.sh
+++ publish.sh
@@ -7,18 +7,33 @@ echo "-------------------------"
 echo "Wiki Publishing"
 echo "-------------------------"
 cd ${GITHUB_PATH}/likejazz.github.io.wiki
-git commit -a -m "Publishing" && git push origin master
+git pull
+git status
 
 # jupyter-notebooks publishing
 echo "-----------------------------"
 echo "jupyter-notebooks Publishing"
 echo "-----------------------------"
 cd ${GITHUB_PATH}/jupyter-notebooks
-git add . && git commit -m "Publishing" && git push origin master
+git pull
+git status
 
 # Github.io publishing
 echo "-------------------------"
 echo "Github.io Publishing"
 echo "-------------------------"
+cd ${GITHUB_PATH}/likejazz.github.io
+git pull
+git status
+
+read -p "Press any key to continue."
+
+# Publishing
+cd ${GITHUB_PATH}/likejazz.github.io.wiki
+git commit -a -m "Publishing" && git push origin master
+
+cd ${GITHUB_PATH}/jupyter-notebooks
+git add . && git commit -m "Publishing" && git push origin master
+
 cd ${GITHUB_PATH}/likejazz.github.io
 git add . && git commit -m "Publishing" && git push origin master
